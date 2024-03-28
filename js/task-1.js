f649fcc1af0a9b8353b13e0e5d928715892e09c2
cel3ntato <adriano2024celentano@gmail.com>
2024-03-28 02:23:59 +0100

    hw-3

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+  const slugArray = title.toLowerCase().split(" ");
+  return (slug = slugArray.join("-"));
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const filteredArray = [];
+  for (const number of numbers) {
+    if (number > value) {
+      filteredArray.push(number);
+    }
+  }
+  return filteredArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
